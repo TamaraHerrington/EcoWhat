ae84f34b3f1da2fd165304dee2fcd7b052ffbb5a
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-07 11:51:45 +0000

    add id to User constructor

--- server/proj/proj/src/main/java/com/capstone/proj/user/User.java
+++ server/proj/proj/src/main/java/com/capstone/proj/user/User.java
@@ -17,11 +17,11 @@ public class User {
     private String email;
     private String password;
 
-    public User(String firstName, String lastName, String email, String password) {
+    public User(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
-
 }
