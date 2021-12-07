da742a83fe8b08e22867cbf51b9a95e14cd4e7f8
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-07 10:59:13 +0000

    make User POJO

---
+++ server/proj/proj/src/main/java/com/capstone/proj/user/User.java
@@ -0,0 +1,27 @@
+package com.capstone.proj.user;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
