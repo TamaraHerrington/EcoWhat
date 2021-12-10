7b5cc909e24eb689194fcd992d825722042b7262
tamaraherrington <tamaraherrington24@gmail.com>
2021-12-10 17:22:16 +0000

    edit CSS hover cards

--- client/my-app/src/components/YouHelp.js
+++ client/my-app/src/components/YouHelp.js
@@ -1,21 +1,24 @@
 import React from "react";
+import Card from "./Card";
 import "./YouHelp.css";
 
 const YouHelp=()=>{
 
     return(
-    
+    <>
         <section class="youhelp">
         <p> For A Better   </p>
-        <div class="scroll-container">
-            <span class="scroll-text">
-                Planet <br></br>
-                Future <br></br>
-                Home
-            </span>
-        </div>
+            <div class="scroll-container">
+                <span class="scroll-text">
+                    Planet <br></br>
+                    Future <br></br>
+                    Home
+                </span>
+            </div>
         </section>
-        
+
+        <Card card />
+        </>
        
     )
 }
