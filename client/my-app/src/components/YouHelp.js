8472b31dfed2f4715b38306369e62942137f1027
tamaraherrington <tamaraherrington24@gmail.com>
2021-12-10 14:40:00 +0000

    fix css scroll effect

--- client/my-app/src/components/YouHelp.js
+++ client/my-app/src/components/YouHelp.js
@@ -1,23 +1,20 @@
 import React from "react";
+import "./YouHelp.css";
 
 const YouHelp=()=>{
 
     return(
         
         <section class="youhelp">
-        <p>This is the </p>
+        <p> For A Better   </p>
         <div class="scroll-container">
             <span class="scroll-text">
-                people <br>
-                water </br>
-                ice
+                Planet <br></br>
+                Future <br></br>
+                Home
             </span>
         </div>
         </section>
-    
-
-
-
 
     )
 }
