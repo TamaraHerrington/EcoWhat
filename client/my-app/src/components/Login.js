6f8fd11ce0f5fe339faf5dffb74b86b4709a3e3e
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-12 19:38:47 +0000

    Login: remove unnecessary navigate and token prop

--- client/my-app/src/components/Login.js
+++ client/my-app/src/components/Login.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Login = ({ onLogin, token }) => {
+const Login = ({ onLogin }) => {
 
-    const navigate = useNavigate()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(null)
