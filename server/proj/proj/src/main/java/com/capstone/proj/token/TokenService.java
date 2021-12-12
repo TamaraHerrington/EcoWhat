e9ec40486345b60b231deb9fb5b41ba4e590af05
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-12 13:19:57 +0000

    make Token POJO and TokenService

---
+++ server/proj/proj/src/main/java/com/capstone/proj/token/TokenService.java
@@ -0,0 +1,13 @@
+package com.capstone.proj.token;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+    @Autowired
+    public TokenService() {}
+
+
+}
