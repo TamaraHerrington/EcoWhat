1b28740c64efd0d9832e824f3829a9761b9a9607
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-13 11:50:13 +0000

    map of england and wales with colour change on hover and popup of constituency name also on hover

--- client/my-app/src/components/Home.js
+++ client/my-app/src/components/Home.js
@@ -1,9 +1,46 @@
+import constituencies from '../data/constituencies.json';
+import {MapContainer, GeoJSON} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Home.css';
 
 const Home = ({ token }) => {
+    const mapStyle = {
+        fillColor: "purple",
+        fillOpacity: 0.6,
+        color: "black",
+        weight: 0.5,
+        
+    }
+
+    const onEachConstituency = (constituency, layer) => {
+        const constituencyName = constituency.properties.PCON13NM;
+        console.log(constituencyName);
+        layer.bindPopup(constituencyName);
+
+        layer.on({
+            click: (event) => {
+                
+            },
+            mouseover: (event) => {
+                event.target.setStyle({fillColor: "red"});
+                layer.bindPopup(constituencyName).openPopup();
+            },
+            mouseout: (event) => {
+                event.target.setStyle({fillColor: "purple"});
+                layer.bindPopup(constituencyName).closePopup(constituencyName);
+            }
+        })
+
+
+    }
 
     return (
         <>
-            <p>Home Page</p>
+            <h1>Home Page</h1>
+            {/* <h1>Constituencies Map</h1> */}
+      <MapContainer style={{height: "85vh"}} zoom={6.5} center={[53,0]}>
+        <GeoJSON className="geo-json" style={mapStyle} data={constituencies.features} onEachFeature={onEachConstituency}/>
+      </MapContainer>
         </>
     )
 }
