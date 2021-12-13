290a15a645ea8addb081526028677fe00d2e4a1c
Vinh Chu <vinhchugg@gmail.com>
2021-12-13 10:49:04 +0000

    Added environment table

---
+++ server/proj/proj/src/main/resources/db/migration/V8__Inserting_Data_To_Enviroment_Data_Table.sql
@@ -0,0 +1,17 @@
+INSERT INTO environment
+(county_name,
+nitrogen_dioxide,
+particulate_matter,
+herbicides,
+fungicides,
+DBPs_winter,
+DBPs_summer)
+VALUES
+("Bedfordshire",
+"22-26",
+"19.75-26.05"
+"81.78-734.9"
+"800.78-7189.3"
+"30.01-40"
+"15.01-30")																	)
+
