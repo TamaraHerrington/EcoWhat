8b645ac057c945e42c43b829b1415311c0ed6043
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-14 22:23:07 +0000

    test: add ValidateEmailTests to ValidatorTest (not exhaustive)

--- server/proj/proj/src/test/java/com/capstone/proj/validator/ValidatorTest.java
+++ server/proj/proj/src/test/java/com/capstone/proj/validator/ValidatorTest.java
@@ -15,6 +15,127 @@ class ValidatorTest {
     }
 
     @Nested
+    class ValidateEmailTests {
+
+        @Test
+        void shouldReturnTrueForValidEmail() {
+            // given
+            String email = "johndoe@email.com";
+
+            // when
+            boolean expected = true;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailIsEmpty() {
+            // given
+            String email = "";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasNoAtSign() {
+            // given
+            String email = "johndoe.com";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasNoDot() {
+            // given
+            String email = "johndoe@email";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasNoTextBeforeAtSign() {
+            // given
+            String email = "@email.com";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasNoTextBetweenAtSignAndDot() {
+            // given
+            String email = "johndoe@.com";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasNoTextAfterDot() {
+            // given
+            String email = "johndoe@email.";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasMultipleAtSigns() {
+            // given
+            String email = "john@doe@email.com";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void shouldReturnFalseIfEmailHasDotBeforeAtSign() {
+            // given
+            String email = "john.doe@email.";
+
+            // when
+            boolean expected = false;
+            boolean actual = validatorTest.validateEmail(email);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
+
+    @Nested
     class ValidatePasswordTests {
 
         @Test
