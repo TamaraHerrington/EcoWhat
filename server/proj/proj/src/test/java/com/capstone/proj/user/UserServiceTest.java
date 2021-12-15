a025413c1c78c63c61661609d81c93f7fd303535
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-15 11:36:23 +0000

    test: add UpdateUserTests and DeleteUserTests to UserServiceTest

--- server/proj/proj/src/test/java/com/capstone/proj/user/UserServiceTest.java
+++ server/proj/proj/src/test/java/com/capstone/proj/user/UserServiceTest.java
@@ -61,7 +61,7 @@ class UserServiceTest {
             int result = userServiceTest.createUser(userBeforeValidation);
 
             // then
-            assertThat(result).isEqualTo(1L);
+            assertThat(result).isEqualTo(1);
 
             ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
             verify(userDAOMock).createUser(userArgumentCaptor.capture());
@@ -390,6 +390,379 @@ class UserServiceTest {
 
             // then
             assertThatThrownBy(() -> userServiceTest.getUserById(1))
+                    .hasMessage("User with id: 1 not found")
+                    .isInstanceOf(ResourceNotFound.class);
+
+            verify(userDAOMock).getUserById(1);
+
+            verifyNoMoreInteractions(userDAOMock);
+        }
+    }
+
+    @Nested
+    class UpdateUserTests {
+
+        @Test
+        void shouldUpdateUserIfInputIsValid() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+            User userAfterValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    null, 100, "Constituency 1", null);
+            User userInDatabase = new User(
+                    1L,
+                    "Johnny", "Doey", "john@doe.com", "P@ssword2",
+                    null, 101, "Constituency 2", null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.of(userInDatabase));
+            when(validatorMock.validateEmail("john@doe.com")).thenReturn(true);
+            when(userDAOMock.getUserByEmail("john@doe.com")).thenReturn(Optional.empty());
+            when(validatorMock.validatePassword("P@ssword1")).thenReturn(true);
+            when(constituencyServiceMock.getConstituencyFromPostcode("Postcode 1")).thenReturn(new Constituency(100, "Constituency 1"));
+            when(userDAOMock.updateUser(1, userAfterValidation)).thenReturn(1);
+
+            // when
+            int result = userServiceTest.updateUser(1, userBeforeValidation);
+
+            // then
+            assertThat(result).isEqualTo(1);
+
+            ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+            ArgumentCaptor<Integer> idArgumentCaptor = ArgumentCaptor.forClass(Integer.class);
+            verify(userDAOMock).updateUser(idArgumentCaptor.capture(), userArgumentCaptor.capture());
+            Integer insertedId = idArgumentCaptor.getValue();
+            assertThat(insertedId).isEqualTo(1);
+            User insertedUser = userArgumentCaptor.getValue();
+            assertThat(insertedUser).isEqualTo(userAfterValidation);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("john@doe.com");
+            verify(userDAOMock).getUserByEmail("john@doe.com");
+            verify(validatorMock).validatePassword("P@ssword1");
+            verify(constituencyServiceMock).getConstituencyFromPostcode("Postcode 1");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoMoreInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfFirstNameIsEmpty() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "", "Doe", "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("First name cannot be empty")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+
+            verifyNoInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfFirstNameIsNull() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    null, "Doe", "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("First name cannot be empty")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+
+            verifyNoInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfLastNameIsEmpty() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "", "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Last name cannot be empty")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+
+            verifyNoInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfLastNameIsNull() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", null, "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Last name cannot be empty")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+
+            verifyNoInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfEmailIsNotValid() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "johndoe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+            when(validatorMock.validateEmail("johndoe.com")).thenReturn(false);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Invalid email address")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("johndoe.com");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfUserWithEmailAlreadyExists() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+
+            User userInDatabase = new User(
+                    2L,
+                    "Johnny", "Doey", "john@doe.com", "P@ssword2",
+                    null, 101, "Constituency 2", null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+            when(validatorMock.validateEmail("john@doe.com")).thenReturn(true);
+            when(userDAOMock.getUserByEmail("john@doe.com")).thenReturn(Optional.of(userInDatabase));
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("User with email already exists")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("john@doe.com");
+            verify(userDAOMock).getUserByEmail("john@doe.com");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfPasswordIsNotValid() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "password",
+                    "Postcode 1", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+            when(validatorMock.validateEmail("john@doe.com")).thenReturn(true);
+            when(userDAOMock.getUserByEmail("john@doe.com")).thenReturn(Optional.empty());
+            when(validatorMock.validatePassword("password")).thenReturn(false);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Invalid password")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("john@doe.com");
+            verify(userDAOMock).getUserByEmail("john@doe.com");
+            verify(validatorMock).validatePassword("password");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfPostcodeIsEmpty() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    "", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+            when(validatorMock.validateEmail("john@doe.com")).thenReturn(true);
+            when(userDAOMock.getUserByEmail("john@doe.com")).thenReturn(Optional.empty());
+            when(validatorMock.validatePassword("P@ssword1")).thenReturn(true);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Postcode cannot be empty")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("john@doe.com");
+            verify(userDAOMock).getUserByEmail("john@doe.com");
+            verify(validatorMock).validatePassword("P@ssword1");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfPostcodeIsNull() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    null, null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+            when(validatorMock.validateEmail("john@doe.com")).thenReturn(true);
+            when(userDAOMock.getUserByEmail("john@doe.com")).thenReturn(Optional.empty());
+            when(validatorMock.validatePassword("P@ssword1")).thenReturn(true);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Postcode cannot be empty")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("john@doe.com");
+            verify(userDAOMock).getUserByEmail("john@doe.com");
+            verify(validatorMock).validatePassword("P@ssword1");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoInteractions(constituencyServiceMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfPostcodeIsNotValid() {
+            // given
+            User userBeforeValidation = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    "Postcode 1", null, null, null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+            when(validatorMock.validateEmail("john@doe.com")).thenReturn(true);
+            when(userDAOMock.getUserByEmail("john@doe.com")).thenReturn(Optional.empty());
+            when(validatorMock.validatePassword("P@ssword1")).thenReturn(true);
+            when(constituencyServiceMock.getConstituencyFromPostcode("Postcode 1"))
+                    .thenThrow(RuntimeException.class);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.updateUser(1, userBeforeValidation))
+                    .hasMessage("Postcode 1 is not a valid postcode")
+                    .isInstanceOf(BadRequest.class);
+
+            verify(userDAOMock).getUserById(1);
+            verify(validatorMock).validateEmail("john@doe.com");
+            verify(userDAOMock).getUserByEmail("john@doe.com");
+            verify(validatorMock).validatePassword("P@ssword1");
+            verify(constituencyServiceMock).getConstituencyFromPostcode("Postcode 1");
+
+            verifyNoMoreInteractions(validatorMock);
+            verifyNoMoreInteractions(userDAOMock);
+            verifyNoMoreInteractions(constituencyServiceMock);
+        }
+    }
+
+    @Nested
+    class DeleteUserTests {
+
+        @Test
+        void shouldDeleteUserIfIdExists() {
+            // given
+            User user = new User(
+                    1L,
+                    "John", "Doe", "john@doe.com", "P@ssword1",
+                    null, 100, "Constituency 1", null);
+
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.of(user));
+            when(userDAOMock.deleteUser(1)).thenReturn(1);
+
+            // when
+            int actual = userServiceTest.deleteUser(1);
+
+            // then
+            assertThat(actual).isEqualTo(1);
+
+            verify(userDAOMock).getUserById(1);
+            verify(userDAOMock).deleteUser(1);
+
+            verifyNoMoreInteractions(userDAOMock);
+        }
+
+        @Test
+        void shouldThrowErrorIfUserWithIdDoesNotExist() {
+            // given
+            when(userDAOMock.getUserById(1)).thenReturn(Optional.empty());
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> userServiceTest.deleteUser(1))
                     .hasMessage("User with id: 1 not found")
                     .isInstanceOf(ResourceNotFound.class);
 
