c38b971a0ff7234ff28191e62fefc1eb9b5be83b
tamaraherrington <tamaraherrington24@gmail.com>
2021-12-15 20:06:45 +0000

    add photo and title to about page

--- client/my-app/src/components/About.js
+++ client/my-app/src/components/About.js
@@ -2,7 +2,24 @@ import React from "react";
 
 const About=()=>{
     return(
+        <>
+        <section class="youhelp">
+            <header className="youhelp__header">
+                <h1 className="youhelp__heading">Meet the Team:  </h1>
+                <div class="scroll-container">
+                    <span class="scroll-text">
+                        Helena <br></br>
+                        Vinh <br></br>
+                        Chaam <br></br>
+                        Jonathan<br></br>
+                        Tamara
+                    </span>
+                </div>
+            </header>
+        </section>
         <h1>About EcoWhat</h1>
+        <img class="image" src={"https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"}/>
+       </>
     )
 }
 
