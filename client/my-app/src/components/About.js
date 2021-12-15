6f6359956534d98a044f6b9a4c674df106447f38
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-15 23:49:05 +0000

    small formatting changes to About and Home

--- client/my-app/src/components/About.js
+++ client/my-app/src/components/About.js
@@ -1,26 +1,26 @@
 import React from "react";
 
-const About=()=>{
+const About = () => {
+
     return(
         <>
-       <section class="youhelp">
-            <header className="youhelp__header">
-                <h1 className="youhelp__heading">For A Better</h1>
-                <div class="scroll-container">
-                    <span class="scroll-text">
-                        Planet <br></br>
-                        Future <br></br>
-                        You
-                    </span>
-                </div>
-            </header>
-        </section>
-        <h1>About EcoWhat</h1>
-        <img class="image" src={"https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"}/>
-      <p>Welcome to EcoWhat? We're a team of eco-concious software engineers looking to make a positive change for the environment.
-          Together we developed EcoWhat? - a website that allows you to get involved in eco-activism. Here you can search for your MP and use our auto-generated templates to ask them to care more for the environemnt. </p>
-       </>
-       
+            <section className="youhelp">
+                <header className="youhelp__header">
+                    <h1 className="youhelp__heading">For A Better</h1>
+                    <div className="scroll-container">
+                        <span className="scroll-text">
+                            Planet <br></br>
+                            Future <br></br>
+                            You
+                        </span>
+                    </div>
+                </header>
+            </section>
+            <h1>About EcoWhat</h1>
+            <img className="image" src={"https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"} alt="Group of people working on laptops"/>
+            <p>Welcome to EcoWhat? We're a team of eco-concious software engineers looking to make a positive change for the environment.
+            Together we developed EcoWhat? - a website that allows you to get involved in eco-activism. Here you can search for your MP and use our auto-generated templates to ask them to care more for the environemnt. </p>
+       </> 
     )
 }
 
