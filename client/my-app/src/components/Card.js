160a1df51685bfa6f1a2b224952b252946c1c108
tamaraherrington <tamaraherrington24@gmail.com>
2021-12-15 13:14:06 +0000

    add website links to cards

--- client/my-app/src/components/Card.js
+++ client/my-app/src/components/Card.js
@@ -4,36 +4,53 @@ const Card = ()=> {
     return(
         <section class="youhelp-card-container">
 
+          
           <section class="youhelp__card youhelp__card--1">
-            <h2 class="youhelp__card-title">Eat Less Meat and Dairy</h2>
+          <a href="https://www.vegansociety.com/">
+          <h2 class="youhelp__card-title">Eat Less Meat and Dairy</h2>
+          </a>
           </section>
 
           <section class="youhelp__card youhelp__card--2">
+            <a href="https://www.recyclenow.com/">
             <h2 class="youhelp__card-title">Recycle</h2>
+            </a>
           </section>
 
           <section class="youhelp__card youhelp__card--3">
+            <a href="https://onlineshop.oxfam.org.uk/second-hand-clothes">
             <h2 class="youhelp__card-title">Buy Second Hand</h2>
+            </a>
           </section>
 
           <section class="youhelp__card youhelp__card--4">
+            <a href="https://learn.eartheasy.com/guides/the-best-eco-friendly-alternatives-for-the-plastic-in-your-life/">
             <h2 class="youhelp__card-title">Ditch Single-Use Plastics</h2>
+            </a>
           </section>
 
           <section class="youhelp__card youhelp__card--5">
+            <a href="https://www.wwf.org.uk/climate-promises">
             <h2 class="youhelp__card-title">Engage in Politics</h2>
+            </a>
           </section>
 
           <section class="youhelp__card youhelp__card--6">
+            <a href="https://sustainabletravel.org/">
             <h2 class="youhelp__card-title">Travel Sustainably</h2>
+            </a>
           </section>
 
           <section class="youhelp__card youhelp__card--7">
+            <a href="https://www.ofwat.gov.uk/households/conservingwater/watersavingtips/">
             <h2 class="youhelp__card-title">Conserve Water</h2>
+            </a>
           </section>
 
           <section class="youhelp__card youhelp__card--8">
+            <a href="https://www.ethicalconsumer.org/palm-oil/palm-oil-free-list">
             <h2 class="youhelp__card-title">Avoid Palm Oil</h2>
+            </a>
           </section>
 
         </section>
