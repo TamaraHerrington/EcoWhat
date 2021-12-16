626abbb70fe2f4a61cb0ed67c432bb7686fbe8fc
helboi4 <h.hopecarter@gmail.com>
2021-12-16 17:01:06 +0000

    added different colours to different categories in comments

--- client/my-app/src/components/comments/Comment.js
+++ client/my-app/src/components/comments/Comment.js
@@ -1,20 +1,54 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons'
+import {useState, useEffect} from "react";
 
 function Comment({token, comment, upvoteComment, downvoteComment}) {
 
+
+    const categorise = (comment) => {
+
+        let category = ""
+
+        switch(comment.comment_category){
+            case "Recycling":
+                category = "comment-category-recycling";
+                break;
+            case "Pollution":
+                category = "comment-category-pollution";
+                break;
+            case "Policy":
+                category = "comment-category-policy";
+                break;
+            case "Energy":
+                category = "comment-category-energy";
+                break;
+            case "Farming":
+                category = "comment-category-farming"
+                break;
+        }
+
+        return category;
+        
+    }
+
+
     return (
-        <div>
-            <h3>{comment.comment_title}</h3>
-            <h3>{comment.comment_category}</h3>
-            <h3>{comment.post_date}</h3>
-            <p>{comment.comment}</p>
+        <div className='comment'>
+            <div className='comment-title'>
+                <h2>{comment.comment_title}</h2>
+                <h2 className={categorise(comment)}>{comment.comment_category}</h2>
+            </div>
+            <h3 className='comment-date'>{comment.post_date}</h3>
+            <p className="comment-body">{comment.comment}</p>
             {upvoteComment && downvoteComment ?
-                <>
+                <div className='comment-ratings'>
                     <h4>{comment.upvotes}</h4>
-                    <button type="button" onClick={() => upvoteComment(token.userId, comment.id)}>Upvote</button>
+                    <button className="comment-votebutton" type="button" onClick={() => upvoteComment(token.userId, comment.id)}><FontAwesomeIcon icon={faThumbsUp} /></button>
                     <h4>{comment.downvotes}</h4>
-                    <button type="button" onClick={() => downvoteComment(token.userId, comment.id)}>Downvote</button>
-                </>
+                    <button className="comment-votebutton" type="button" onClick={() => downvoteComment(token.userId, comment.id)}><FontAwesomeIcon icon={faThumbsDown} /></button>
+                </div>
                 :
                 <></>
             }
