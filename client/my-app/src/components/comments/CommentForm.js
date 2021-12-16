71b7581849c8a70c79f24615c12488bb64ac063d
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-16 09:38:32 +0000

    fix: comment form doesn't break if not logged in, About goes to /about in mobile view

--- client/my-app/src/components/comments/CommentForm.js
+++ client/my-app/src/components/comments/CommentForm.js
@@ -31,33 +31,23 @@ function CommentForm({ getComments, token, currentConstituency }) {
     //TODO: Add some logic for content checking here (swearing, banned words, etc)
     const handleCommentSubmit = (event) => {
         event.preventDefault();
-        const today = new Date();
-        const dateTime = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        console.log(title)
-        console.log(comment)
-        console.log(user.id)
-        console.log(category)
-        console.log(typeof(category))
-        console.log(currentConstituency.constituency_id)
-        console.log(dateTime)
 
-        const commentToSubmit = {
-                        "userId" : user.id,
-                        "comment_title" : title,
-                        "comment": comment,
-                        "comment_category": category,
-                        "constituencyId": currentConstituency.constituency_id,
-                        "post_date": dateTime
-                    };
-
-        const commentToSubmitJSON = JSON.stringify(commentToSubmit) 
-
-        console.log(commentToSubmitJSON)
-
-        console.log(commentToSubmit)
-        console.log(typeof(commentToSubmit))
-        
         if(comment && title && token){
+            const today = new Date();
+            const dateTime = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            
+            const commentToSubmit = {
+                "userId" : user.id,
+                "comment_title" : title,
+                "comment": comment,
+                "comment_category": category,
+                "constituencyId": currentConstituency.constituency_id,
+                "post_date": dateTime
+            };
+
+            console.log(commentToSubmit)
+            const commentToSubmitJSON = JSON.stringify(commentToSubmit) 
+        
             fetch(
                 "http://localhost:8080/api/comments/add",
                 {
@@ -89,26 +79,26 @@ function CommentForm({ getComments, token, currentConstituency }) {
 
     return (
         <form className="comment-form" onSubmit={handleCommentSubmit}>
-                    <h1 className="comment-header">Add Comment</h1>
+            <h1 className="comment-header">Add Comment</h1>
 
-                    <label forhtml="title">Title</label>
-                    <input type="text" id="comment-title" onChange={(event) => setTitle(event.target.value)}/>
+            <label forhtml="title">Title</label>
+            <input type="text" id="comment-title" onChange={(event) => setTitle(event.target.value)}/>
 
-                    <label forhtml="category">Choose a relevant category</label>
-                    <select id="categories" name="categories" onChange={(event) => setCategory(event.target.value)}>
-                        <option value="Recycling">Recycling</option>
-                        <option value="Pollution">Pollution</option>
-                        <option value="Policy">Policy</option>
-                        <option value="Energy">Energy</option>
-                        <option value="Farming">Farming</option>
-                    </select>
+            <label forhtml="category">Choose a relevant category</label>
+            <select id="categories" name="categories" onChange={(event) => setCategory(event.target.value)}>
+                <option value="Recycling">Recycling</option>
+                <option value="Pollution">Pollution</option>
+                <option value="Policy">Policy</option>
+                <option value="Energy">Energy</option>
+                <option value="Farming">Farming</option>
+            </select>
 
-                    <label forhtml="comment_body">Comment</label>
-                    <input type="text" id="comment-text" onChange={(event) => setComment(event.target.value)}/>
+            <label forhtml="comment_body">Comment</label>
+            <input type="text" id="comment-text" onChange={(event) => setComment(event.target.value)}/>
 
-                    <input type="submit" value="Leave comment"/>
-                    
-                </form>
+            <input type="submit" value="Leave comment"/>
+            
+        </form>
         
     )
 }
